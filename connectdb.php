3e2828f884cead8ac10298a714bd1dd5df4e698e
Callum <220115122@aston.ac.uk>
2023-11-24 12:35:44 +0000

    connectdb creation, and signOut + connectdb code

---
+++ connectdb.php
@@ -0,0 +1,15 @@
+<?php
+
+$db_host = 'localhost';
+$db_name = '';
+$username = 'root';
+$password = '';
+
+try{
+    $db = new PDO("mysql:dbname=$db_name;host=$db_host", $username, $password);
+} catch(PDOException $ex){
+    echo("Failed to connect to the database.<br>");
+    echo($ex->getMessage());
+}
+
+?>
