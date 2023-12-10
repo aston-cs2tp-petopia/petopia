83a6f333c00b9ae39ca8a6910018d2a6481844d8
Callum <220115122@aston.ac.uk>
2023-12-10 17:26:30 +0000

    Database is imported into products.php

--- php/connectdb.php
+++ php/connectdb.php
@@ -1,9 +1,14 @@
 <?php
 
-$db_host = '134.151.21.205';
-$db_name = 'u_210075069_petopiadb';
-$username = 'u-210075069';
-$password = 'nPcCNcViPrL7oBX';
+// $db_host = 'cs2410-web01pvm.aston.ac.uk';
+// $db_name = 'u_210075069_petopiadb';
+// $username = 'u-210075069';
+// $password = 'VY7WdmJnGlj/m02g';
+
+$db_host = 'localhost';
+$db_name = 'petopia2';
+$username = 'root';
+$password = '';
 
 try{
     $db = new PDO("mysql:dbname=$db_name;host=$db_host", $username, $password);
