adfdfe49ba8482013757a61acf726390a63b247c
HBagash <220055806@aston.ac.uk>
2023-12-11 08:11:20 +0000

    Basket Styles + PHP
    Remove basket PHP
    Styled Basket page
    Fixed Navigation issues with basket

--- advice.php
+++ advice.php
@@ -43,40 +43,45 @@
         <!--Middle Navigation-->
         <nav class="desktop-nav">
             <ul class="desktop-nav-ul">
-                <li><a href="index.html">Home</a></li>
+                <li><a href="index.php">Home</a></li>
                 <!--Dropdown-->
                 <li class="dropdown">
                     <a href="#">Pets v</a>
                     <ul class="dropdown-menu">
-                        <li class="dropdown-li"><a href="items.html">Cats</a></li>
-                        <li class="dropdown-li"><a href="items.html">Dogs</a></li>
+                        <li class="dropdown-li"><a href="products.php">Cats</a></li>
+                        <li class="dropdown-li"><a href="products.php">Dogs</a></li>
                     </ul>
                 </li>
                 <!--Dropdown-->
                 <li class="dropdown">
                     <a href="#">Shop v</a>
                     <ul class="dropdown-menu">
-                        <li class="dropdown-li"><a href="items.html">Toys</a></li>
-                        <li class="dropdown-li"><a href="items.html">Grooming</a></li>
-                        <li><a href="items.html">Treats</a></li>
+                        <li class="dropdown-li"><a href="products.php">Toys</a></li>
+                        <li class="dropdown-li"><a href="products.php">Grooming</a></li>
+                        <li><a href="products.php">Treats</a></li>
                     </ul>
                 </li>
-                <li><a href="advice.html">Advice</a></li>
-                <li><a href="about.html">About Us</a></li>
-                <li><a href="contact.html">Contact</a></li>
+                <li><a href="advice.php">Advice</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="contact.php">Contact</a></li>
             </ul>
         </nav>
 
         <!--Right Navigation-->
         <div class="right-nav">
             <!--Shoppiung Basket Button-->
-            <a href="" class="basket-link">
-                <div class='basket-button bx bx-basket'></div>
-            </a>
-            <!--Login Button-->
-            <a href="login.php" class="login-link">
-                <div class="login-button">Login</div>
-            </a>
+            <?php
+                //Login Button
+                if ($b==true) {
+                    //Log out button
+                    //Shoppiung Basket Button
+                    echo '<a href="basket.php" class="basket-link"><div class="basket-button bx bx-basket"></div></a>';
+                    echo '<div class="login-button"><a href="php/signOut.php"">Log Out</a></div>';
+                }else{
+                    //Login Button
+                    echo '<div class="login-button"><a href="login.php">Login</a></div>';
+                }       
+            ?>
         </div>
 
         <!--Mobile-Background-->
@@ -88,35 +93,39 @@
                 <p class="close-menu-button" draggable="false">X</p>
             </div>
             <ul>
-                <li><a href="index.html">Home</a></li>
+                <li><a href="index.php">Home</a></li>
                 <!--Dropdown-->
                 <li class="dropdown">
                     <a href="#">Pets v</a>
                     <ul class="dropdown-menu-mobile">
-                        <li class="dropdown-li"><a href="items.html">Cats</a></li>
-                        <li class="dropdown-li"><a href="items.html">Dogs</a></li>
+                        <li class="dropdown-li"><a href="products.php">Cats</a></li>
+                        <li class="dropdown-li"><a href="products.php">Dogs</a></li>
                     </ul>
                 </li>
                 <!--Dropdown-->
                 <li class="dropdown">
                     <a href="#">Shop v</a>
                     <ul class="dropdown-menu-mobile">
-                        <li class="dropdown-li"><a href="items.html">Cats</a></li>
-                        <li class="dropdown-li"><a href="items.html">Dogs</a></li>
+                        <li class="dropdown-li"><a href="products.php">Cats</a></li>
+                        <li class="dropdown-li"><a href="products.php">Dogs</a></li>
                     </ul>
                 </li>
-                <li><a href="advice.html">Advice</a></li>
-                <li><a href="about.html">About Us</a></li>
-                <li><a href="contact.html">Contact</a></li>
+                <li><a href="advice.php">Advice</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="contact.php">Contact</a></li>
                 <div class="mobile-bottom-nav">
-                    <!--Login Button-->
-                    <a href="login.php" class="login-link">
-                        <div class="login-button">Login</div>
-                    </a>
-                    <!--Shoppiung Basket Button-->
-                    <a href="" class="basket-link">
-                        <div class='basket-button bx bx-basket'></div>
-                    </a>
+                    <?php
+                //Login Button
+                if ($b==true) {
+                    //Log out button
+                    //Shoppiung Basket Button
+                    echo '<a href="basket.php" class="basket-link"><div class="basket-button bx bx-basket"></div></a>';
+                    echo '<div class="login-button"><a href="php/signOut.php"">Log Out</a></div>';
+                }else{
+                    //Login Button
+                    echo '<div class="login-button"><a href="login.php">Login</a></div>';
+                }       
+            ?>
                 </div>
             </ul>
         </nav>
