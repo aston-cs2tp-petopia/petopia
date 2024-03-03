5c7100712e151f9eca4370bf6e064acab90ae13a
Maibby1204 <bayang1204@gmail.com>
2024-03-03 12:04:10 +0000

    update to items and products

--- products.php
+++ products.php
@@ -1,8 +1,8 @@
 <?php
  require_once('php/mainLogCheck.php');
 
-    $productType=$_GET["productType"];
-    echo($productType);
+    //$productType=$_GET["productType"];
+    //echo($productType);
     
     //when the basket button is pressed, send the product id and customer id to order details
     if (isset($_POST['add'])) {
@@ -46,8 +46,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Petopia</title>
     <link href="css/items.css" rel="stylesheet" type="text/css">
-    <link href="css/navigation.css" rel="stylesheet" type="text/css">
-    <link href="css/footer.css" rel="stylesheet" type="text/css">
 
     <!--[Google Fonts]-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -60,120 +58,32 @@
     <!--Box Icons-->
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
-    <!--Flickity-->
+    <!--
+        [Navigation & Footer]
+    -->
+    <script src="templates/navigationTemplate.js"></script>
+    <link href="css/navigation.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/footer.css">
+
     <!--CSS Templates-->
     <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
     <link rel="stylesheet" href="templates/hero-banner.css">
+
     <!--JS-->
     <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
 
 </head>
 
 <body>
-    <header>
         <!--
         [NAVIGATION/HEADER]
     -->
     <header>
-        <!--Logo-->
-        <div class="logo-container"><a href="#"><img src="assets/logo.png" alt=""></a></div>
 
-        <!--Middle Navigation-->
-        <nav class="desktop-nav">
-            <ul class="desktop-nav-ul">
-                <li><a href="index.php">Home</a></li>
-                <!--Dropdown-->
-                <li class="dropdown">
-                    <a href="#">Pets v</a>
-                    <ul class="dropdown-menu">
-                        <li class="dropdown-li"><a href="products.php">Cats</a></li>
-                        <li class="dropdown-li"><a href="products.php">Dogs</a></li>
-                    </ul>
-                </li>
-                <!--Dropdown-->
-                <li class="dropdown">
-                    <a href="#">Shop v</a>
-                    <ul class="dropdown-menu">
-                        <li class="dropdown-li"><a href="products.php">Toys</a></li>
-                        <li class="dropdown-li"><a href="products.php">Grooming</a></li>
-                        <li><a href="products.php">Treats</a></li>
-                    </ul>
-                </li>
-                <li><a href="advice.php">Advice</a></li>
-                <li><a href="about.php">About Us</a></li>
-                <li><a href="contact.php">Contact</a></li>
-            </ul>
-        </nav>
-
-        <!--Right Navigation-->
-        <div class="right-nav">
-            <!--Shoppiung Basket Button-->
-            <?php
-                //Login Button
-                if ($b==true) {
-                    //Log out button
-                    //Shoppiung Basket Button
-                    echo '<a href="basket.php" class="basket-link"><div class="basket-button bx bx-basket"></div></a>';
-                    echo '<div class="login-button"><a href="php/signOut.php"">Log Out</a></div>';
-                }else{
-                    //Login Button
-                    echo '<div class="login-button"><a href="login.php">Login</a></div>';
-                }       
-            ?>
-        </div>
-
-        <!--Mobile-Background-->
-        <div class="mobile-background"></div>
-        <!--Mobile Navigation-->
-        <nav class="mobile-nav">
-            <div class="mobile-nav-top">
-                <div class="mobile-logo"><img src="assets/logo.png" alt=""></div>
-                <p class="close-menu-button" draggable="false">X</p>
-            </div>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <!--Dropdown-->
-                <li class="dropdown">
-                    <a href="#">Pets v</a>
-                    <ul class="dropdown-menu-mobile">
-                        <li class="dropdown-li"><a href="products.php">Cats</a></li>
-                        <li class="dropdown-li"><a href="products.php">Dogs</a></li>
-                    </ul>
-                </li>
-                <!--Dropdown-->
-                <li class="dropdown">
-                    <a href="#">Shop v</a>
-                    <ul class="dropdown-menu-mobile">
-                        <li class="dropdown-li"><a href="products.php">Cats</a></li>
-                        <li class="dropdown-li"><a href="products.php">Dogs</a></li>
-                    </ul>
-                </li>
-                <li><a href="advice.php">Advice</a></li>
-                <li><a href="about.php">About Us</a></li>
-                <li><a href="contact.php">Contact</a></li>
-                <div class="mobile-bottom-nav">
-                    <?php
-                //Login Button
-                if ($b==true) {
-                    //Log out button
-                    //Shoppiung Basket Button
-                    echo '<a href="basket.php" class="basket-link"><div class="basket-button bx bx-basket"></div></a>';
-                    echo '<div class="login-button"><a href="php/signOut.php"">Log Out</a></div>';
-                }else{
-                    //Login Button
-                    echo '<div class="login-button"><a href="login.php">Login</a></div>';
-                }       
-            ?>
-                </div>
-            </ul>
-        </nav>
-        <!--Mobile Hamburger-->
-        <div id="hamburger-button" class='bx bx-menu'></div>
     </header>
     <!--
         [HEADER/NAVIGATION END]
     -->
-    </header>
     
     <main>
         <!--Hero Banner-->
@@ -190,23 +100,71 @@
                 </div>
             </div>
         </section>
+
         <section class="items-container">
-            <?php
+            <div class="items-left">
+
+            </div>
+
+            <div class="items-right">
+
+                <div class="filters-top">
+                    <!--Filters-->
+                    <div class="filters-left">
+                        <!--Show N per page-->
+                        <div class="show-list-container">
+                            <form><label for="show">Show</label>
+                                <select id="show" class="selected">
+                                    <option value="true">06</option>
+                                    <option value="true">12</option>
+                                    <option value="true">18</option>
+                                    <option value="true">24</option>
+                                    <option value="true">30</option>
+                                </select>
+                            </form>
+                        </div>
+
+                        <!--Sort By-->
+                        <div class="sort-by-container">
+                            <form><label for="shortBy">Sort By</label>
+                                <select id="shortBy" class="selected">
+                                    <option value="true">Select</option>
+                                    <option value="true">Low to high</option>
+                                    <option value="true">High to low</option>
+                                </select>
+                            </form>
+                        </div>
+                    </div>
+
+                    <!--Search-->
+                    <form id="filters-right">
+                        <!--Search Input Container-->
+                        <div class="input-container">
+                            <input type="text" id="contact-name" name="contact-name" class="name-input"
+                                placeholder="Search..." required />
+                            <i class='bx bx-search'></i>
+                        </div>
+                    </form>
+                </div>
+
+                    <div class="results-container">
+                    <?php
                 require_once('php/connectdb.php');
                 try {
-                    $productQuery = "select * from  product where "; //need to add 'where' query once i have a category variable
+                    $productQuery = "select * from  product"; //WHERE //need to add 'where' query once i have a category variable
                     $rows =  $db->query($productQuery);
 
                     //display the query edited table	
                     if ($rows && $rows->rowCount() > 0) {
                         foreach ($rows as $row) {
-            ?>
+                    ?>
                             <div class="item-template">
                                 <div class="item-image">
                                     <img src="assets/Homepage/hero-banner2.jpg" alt="">
                                 </div>
                             
                                 <div class="item-info">
+                                    <h6>Category</h6>
                                     <?php $tempPID=$row['Product_ID']  ?>
                                     <h4><a href="item.php?Product_ID=<?php echo $tempPID; ?>"><?php echo $row['Name']; ?></a></h4>
                                     <!-- <td align="left"><a href="projectdetails.php?pid=' . $pidTemp . '"> -->
@@ -219,23 +177,26 @@
                                                 echo "<form method='post' action='products.php'>";
                                                 echo '<input type="hidden" name="productID" value="' . $row['Product_ID'] . '">';
                                                 echo '<input type="hidden" name="quantity" value="1">';
-                                                echo '<button type="submit" name="add"><div class="bx bx-cart-add"></div></button>';
+                                                echo '<button class="add-cart-btn" type="submit" name="add"><div class="bx bx-cart-add"></div></button>';
                                                 echo '</form>';
                                             }   
                                         ?>
                                     </div>
                                 </div>
                             </div>
-            <?php
-                        }
-                    } else {
-                        echo  "<p>No matching Product.</p>\n"; //no match found
-                    }
-                } catch (PDOexception $ex) {
-                    echo "Sorry, a database error occurred! <br>";
-                    echo "Error details: <em>" . $ex->getMessage() . "</em>";
-                }
-            ?>
+                        <?php
+                                    }
+                                } else {
+                                    echo  "<p>No matching Product.</p>\n"; //no match found
+                                }
+                            } catch (PDOexception $ex) {
+                                echo "Sorry, a database error occurred! <br>";
+                                echo "Error details: <em>" . $ex->getMessage() . "</em>";
+                            }
+                        ?>
+
+                </div>
+            </div>
         </section>
         </main>
 </body>
