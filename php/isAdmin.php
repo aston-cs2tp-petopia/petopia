306f191f7ee9c0a6fe896ab3f7066c4b72591d94
HBagash <220055806@aston.ac.uk>
2024-03-21 19:16:37 +0000

    Update isAdmin.php

--- php/isAdmin.php
+++ php/isAdmin.php
@@ -9,8 +9,6 @@ if (isset($_SESSION['username'])) {
     $result=$query->fetch(PDO::FETCH_ASSOC);
 
     if ($result['Is_Admin']==2){
-        echo $result['Is_Admin'];
-        echo"here";
         return true;
     } else {
         return false;
